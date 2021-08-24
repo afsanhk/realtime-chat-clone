343976be2c37eaecd659113a8e73e6ccb8f6e06c
Afsanul Haque Khan <afsanhk@gmail.com>
2021-08-24 18:51:25 -0400

    Set up socket connection and disconnection in server/index.js and basic routes in router.js

--- server/index.js
+++ server/index.js
@@ -6,8 +6,22 @@ const http = require("http");
 
 const PORT = process.env.PORT || 5000;
 
+const router = require("./router");
+
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server); // This is an instance of socketio
+
+// Run all socket code inside the io.on wrapper.
+// Important to pass in socket here, this is unique to each connection.
+io.on("connection", (socket) => {
+  console.log("We have a new connection!");
+  // Notice that this uses socket.on instead of io.on -> acting on just one socket connection
+  socket.on("disconnect", () => {
+    console.log("User has left!");
+  });
+});
+
+app.use(router);
 
 server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
