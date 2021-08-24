63ea62158a55465141a110e4f9fff116c7e43b53
Afsanul Haque Khan <afsanhk@gmail.com>
2021-08-24 18:28:54 -0400

    Set up basic index.js code with import React, ReactDOM, App and ReactDOM.render(...)

--- client/src/index.js
+++ client/src/index.js
@@ -2,3 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
+
+// Basic boilerplate code to inject everything into the root div in public/index.html.
+ReactDOM.render(<App />, document.querySelector("#root"));
