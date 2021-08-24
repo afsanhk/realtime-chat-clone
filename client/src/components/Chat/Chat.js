4e7fa8b34b2840ecdb4793df7ea4825f128cb178
Afsanul Haque Khan <afsanhk@gmail.com>
2021-08-24 19:30:26 -0400

    Add cors safe route for localhost:3002 to server/index.js so socket can send messages to server properly. Next step is to properly set up custom socket.emit(client-side) and socket.on(server-side) events.

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import queryString from "query-string"; // Used to retrieve data from URL
+import io from "socket.io-client"; // Browser-side socket.io
 
 import "./Chat.css";
 
-const Chat = () => {
+// Initialized outside of function
+let socket;
+
+const Chat = ({ location }) => {
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+  const ENDPOINT = "localhost:5000";
+
+  useEffect(() => {
+    // Takes the name and room entered on the Join page, destructures it and sets it to state
+    const { name, room } = queryString.parse(location.search); // location comes from react-router as a prop
+
+    socket = io(ENDPOINT); // We need to pass an endpoint to the server (hosted on localhost:5000)
+
+    setName(name);
+    setRoom(room);
+
+    // You can emit events from clientside to the server side
+    socket.emit("join", { name, room });
+  }, [ENDPOINT, location.search]);
+
   return <h1>Chat</h1>;
 };
 
--- server/index.js
+++ server/index.js
@@ -10,12 +10,18 @@ const router = require("./router");
 
 const app = express();
 const server = http.createServer(app);
-const io = socketio(server); // This is an instance of socketio
+const io = socketio(server, { cors: { origin: "http://localhost:3002" } }); // This is an instance of socketio, need to set up cors origin in case socket and server are not served by same port
 
 // Run all socket code inside the io.on wrapper.
 // Important to pass in socket here, this is unique to each connection.
 io.on("connection", (socket) => {
   console.log("We have a new connection!");
+
+  // Client emits a specific event, server does something on this event
+  socket.on("join", ({ name, room }) => {
+    console.log(name, room);
+  });
+
   // Notice that this uses socket.on instead of io.on -> acting on just one socket connection
   socket.on("disconnect", () => {
     console.log("User has left!");
